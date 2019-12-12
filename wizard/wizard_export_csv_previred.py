cd4dd6c0d0eac244073e90c524359b68f9123ebf
RetargetingCL <54725593+RetargetingCL@users.noreply.github.com>
2019-12-12 17:50:48 -0300

    probando

--- wizard/wizard_export_csv_previred.py
+++ wizard/wizard_export_csv_previred.py
@@ -124,9 +124,8 @@ class WizardExportCsvPrevired(models.TransientModel):
     @api.model
     def validity_date_start(self, payslip):
         if payslip.date_start_mp: 
-            logging.info(payslip.date_start_mp.strftime("%d/%m/%Y") )
-            logging.info('fechaaaaaaaaaaa')
-            return payslip.date_start_mp.strftime("%d/%m/%Y") 
+            fecha = payslip.date_start_mp.strftime("%d/%m/%Y")           
+            return fecha
         else:
             if payslip.movimientos_personal != '0' and not payslip.date_start_mp:
                 return payslip.date_from.strftime("%d/%m/%Y")   
@@ -136,7 +135,8 @@ class WizardExportCsvPrevired(models.TransientModel):
     @api.model
     def validity_date_end(self, payslip):
         if payslip.date_end_mp:
-            return payslip.date_end_mp.strftime("%d/%m/%Y")   
+            fecha =payslip.date_end_mp.strftime("%d/%m/%Y")  
+            return fecha 
         else:
             if payslip.movimientos_personal != '0' and not payslip.date_end_mp:
                 return payslip.date_from.strftime("%d/%m/%Y")   
